454c9a3664342258f56b298b324f5c2c66f284d1
alphadrow <96702203+alphadrow@users.noreply.github.com>
2022-06-05 16:42:06 +0300

    Add files via upload

---
+++ scheme.sql
@@ -0,0 +1,58 @@
+CREATE TYPE "genre" AS ENUM (
+  'COMEDEY',
+  'DRAMA',
+  'CARTOON',
+  'THRILLER',
+  'DOCUMENTARY',
+  'ACTION'
+);
+
+CREATE TYPE "mpa" AS ENUM (
+  'G',
+  'PG',
+  'PG13',
+  'R',
+  'NC'
+);
+
+CREATE TABLE "users" (
+  "user_id" int4 PRIMARY KEY,
+  "email" varchar(50),
+  "login" varchar(30),
+  "name" varchar(100),
+  "birthday" date
+);
+
+CREATE TABLE "friends" (
+  "user_id" int4,
+  "friend_id" int4
+);
+
+CREATE TABLE "likes" (
+  "user_id" int4,
+  "film_id" int4
+);
+
+CREATE TABLE "films" (
+  "film_id" int4 PRIMARY KEY,
+  "name" varchar(50),
+  "description" varchar(150),
+  "release_date" date,
+  "duration" int2,
+  "rate" int4,
+  "mpa" mpa,
+  "likes_count" int4
+);
+
+CREATE TABLE "genres" (
+  "film_id" int4,
+  "genre" genre
+);
+
+ALTER TABLE "genres" ADD FOREIGN KEY ("film_id") REFERENCES "films" ("film_id");
+
+ALTER TABLE "friends" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "likes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "likes" ADD FOREIGN KEY ("film_id") REFERENCES "films" ("film_id");
